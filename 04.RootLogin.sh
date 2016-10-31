a1451d7928263c21848b054823799694357547ea
zerocool (ByeongRok Kim) <sparcs@naver.com>
2016-10-31 09:36:54 +0900

    Update 04.RootLogin.sh

--- 04.RootLogin.sh
+++ 04.RootLogin.sh
@@ -1,6 +1,6 @@
 ## 04. Root Login
 
-AUTOSET_HOME=/home/pi/Retropie-AutoSet
+AUTOSET_HOME=/home/pi/RetroPie-AutoSet
 
 sed -i "s/^PermitRootLogin without-password/PermitRootLogin yes/" /etc/ssh/sshd_config
 #ps -ef | grep -v grep | grep root | grep sshd | awk '{print "kill -HUP "$2}' | sh
