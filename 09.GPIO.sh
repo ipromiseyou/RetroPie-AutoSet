cfb43c92cf96795833aff8fc00bd02e2af807886
zerocool (ByeongRok Kim) <sparcs@naver.com>
2016-10-31 19:53:10 +0900

    Update 09.GPIO.sh

--- 09.GPIO.sh
+++ 09.GPIO.sh
@@ -5,7 +5,9 @@ AUTOSET_HOME=/home/pi/RetroPie-AutoSet
 sudo apt-get update
 sudo apt-get -qy upgrade
 sudo apt-get install -y --force-yes dkms cpp-4.7 gcc4.7 git joystick
-sudo apt-get install raspberrypi-kernel-headers
+wget http://www.niksula.hut.fi/~mhiienka/Rpi/linux-headers-rpi/linux-headers-`uname -r`_`uname -r`-2_armhf.deb
+sudo dpkg -i linux-headers-`uname -r`_`uname -r`-2_armhf.deb
+sudo rm linux-headers-`uname -r`_`uname -r`-2_armhf.deb
 wget https://github.com/digitalLumberjack/mk_arcade_joystick_rpi/releases/download/v0.1.4/mk-arcade-joystick-rpi-0.1.4.deb
 sudo dpkg -i mk-arcade-joystick-rpi-0.1.4.deb
 echo "mk_arcade_joystick_rpi" >> /etc/modules
